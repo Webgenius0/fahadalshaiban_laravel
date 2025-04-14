70f6397bedf797d03be3414ae81d1fa7cede341e
isratahamedsabbir <93650049+isratahamedsabbir@users.noreply.github.com>
2025-04-14 18:14:23 +0600

    u

--- app/Http/Controllers/Web/Auth/OtpVerificationController.php
+++ app/Http/Controllers/Web/Auth/OtpVerificationController.php
@@ -39,9 +39,8 @@ class OtpVerificationController extends Controller
             'user_id' => $user->id
         ]);
 
-        // dd($otp);
-        $mail = Mail::to($user->email)->send(new OtpMail($otp->code));
-        // dd($mail);
+        Mail::to($user->email)->send(new OtpMail($otp->code));
+
         return view('auth.otp');
     }
 
--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -37,7 +37,7 @@ class OtpMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.otp',
+            view: 'mail.otpmail',
             with: [
                 'otp' => $this->otp,
             ],
