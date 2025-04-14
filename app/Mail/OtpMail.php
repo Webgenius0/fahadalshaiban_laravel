13b55d4a36680eb8f17c5dba779a76a333d8b094
MuhammadSagorwdpf <sagorwdpf@gmail.com>
2025-04-14 13:10:25 +0600

    Update OTP email view path from 'auth.otp' to 'emails.otp'

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -37,7 +37,7 @@ class OtpMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'auth.otp',
+            view: 'emails.otp',
             with: [
                 'otp' => $this->otp,
             ],
